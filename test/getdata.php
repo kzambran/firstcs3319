684227eb8e1968cb089f726f76eb0601d8c2d2fd
Katherine <katty2254@hotmail.com>
2018-12-05 23:33:58 -0500

    deleted test

--- test/getdata.php
+++
@@ -1,6 +0,0 @@
-<?php
-  echo '<input type="radio" name="Order" value="byName">';
-  echo "Order by team name</br>";
-  echo '<input type="radio" name="Order" value="byCity">';
-  echo "Order by team city</br>";
-?>
