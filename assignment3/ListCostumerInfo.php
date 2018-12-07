a68638d89a934688def25a94074f4f23048e5dca
Katherine <katty2254@hotmail.com>
2018-12-06 22:08:12 -0500

    update

--- assignment3/ListCostumerInfo.php
+++ assignment3/ListCostumerInfo.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 	<title>Store</title>
-	<link rel="stylesheet" type="text/css" href="store.css">
-	<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
 </head>
 <body>
 <?php
-include"connecttodb.php";
+include 'connecttodb.php';
 ?>
 <h1>My store</h1>
 Costumers information:
 <?php
-include'getCostumerInfo.php';
+include 'getCostumerInfo.php';
 ?>
+<ol>
+    <li>costumers</li>
+</ol>
 </body>
 </html>
